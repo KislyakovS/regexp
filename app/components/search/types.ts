7ca1addf7baf16ccdebf4aa67fcf646ee9a413c6
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-12-26 15:31:52 +0300

    add component Search

---
+++ app/components/search/types.ts
@@ -0,0 +1,5 @@
+import { HTMLAttributes, DetailedHTMLProps } from 'react';
+
+type Default = DetailedHTMLProps<HTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+
+export interface Props extends Default { }
