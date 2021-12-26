be613e2dc020140d25f29bd2d5f087d0586bab27
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-12-26 14:20:31 +0300

    add global styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+// Core
 import {
   Links,
   LiveReload,
@@ -6,11 +7,23 @@ import {
   Scripts,
   ScrollRestoration
 } from "remix";
-import type { MetaFunction } from "remix";
+import type { MetaFunction, LinksFunction } from "remix";
+
+// Styles
+import globalStyles from './styles/global.css';
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+    { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap' },
+    { rel: 'stylesheet', href: globalStyles },
+  ];
+}
 
 export default function App() {
   return (
@@ -22,7 +35,9 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <main>
+          <Outlet />
+        </main>
         <ScrollRestoration />
         <Scripts />
         {process.env.NODE_ENV === "development" && <LiveReload />}
