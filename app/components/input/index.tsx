a498e7356a544f29540fa1c8384c362153c87a16
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-12-28 19:15:24 +0300

    add props `isCopy` for component `Input`

--- app/components/input/index.tsx
+++ app/components/input/index.tsx
@@ -1,14 +1,29 @@
 // Core
-import { forwardRef } from 'react';
+import { forwardRef, useCallback, MouseEvent } from 'react';
 import clsx from 'clsx';
 
 // Types
 import type { Props } from './types';
 
-const Input = forwardRef<HTMLInputElement, Props>(({ className, ...props }, ref) => {
-  const inputClasses = clsx(className, 'input');
+const Input = forwardRef<HTMLInputElement, Props>(({ className, isCopy, ...props }, ref) => {
+  const inputClasses = clsx(className, 'input', isCopy && 'input--pointer');
 
-  return <input className={inputClasses} ref={ref} {...props} />;
+  const onClickCopy = useCallback((event: MouseEvent<HTMLInputElement>) => {
+    const { value } = event.target as HTMLInputElement
+    navigator.clipboard.writeText(value);
+
+    if (props.onClick) {
+      props.onClick(event);
+    }
+
+  }, [])
+
+  return <input
+    {...props}
+    className={inputClasses}
+    ref={ref}
+    onClick={isCopy ? onClickCopy : undefined}
+  />;
 });
 
 export default Input;
--- app/components/input/types.ts
+++ app/components/input/types.ts
@@ -2,4 +2,6 @@ import { HTMLAttributes, DetailedHTMLProps } from 'react';
 
 type Default = DetailedHTMLProps<HTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
-export interface Props extends Default { }
+export interface Props extends Default {
+  isCopy?: boolean;
+}
