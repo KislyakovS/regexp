a4a979e5eab2ae0ae067e0163a091bbefb8d2c15
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-01-02 18:28:37 +0300

    add list regexp

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,22 @@
-export default function Index() {
+// Components
+import Card from '../components/card';
+
+// Assets
+import regexps from '~/assets/regexp.json';
+
+export default () => {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+    <ul className="regexps">
+      {regexps.map(({ title, regexp, example, tags }) => (
+        <li key={regexp}>
+          <Card
+            title={title}
+            regexp={regexp}
+            example={example}
+            tags={tags}
+          />
         </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+      ))}
+    </ul>
+  )
 }
