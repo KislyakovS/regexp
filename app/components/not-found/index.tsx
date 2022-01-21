c71d1f938d48e2f8e194a2aebb7b7f842d7f874c
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-01-21 19:00:21 +0300

    remove style in component NotFound

--- app/components/not-found/index.tsx
+++ app/components/not-found/index.tsx
@@ -3,9 +3,9 @@ import { FC } from 'react';
 
 const NotFound: FC = () => {
   return (
-    <div className="not-found">
-      <h5 className="not-found__code">404</h5>
-      <h2 className="not-found__message">Whoops! This is not what you were looking for</h2>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <h5 className="text-gray-900 text-lg font-medium">404</h5>
+      <h2 className="mt-1 text-5xl font-bold">Whoops! This is not what you were looking for</h2>
     </div>
   )
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         100: '#f4f4ff',
         200: '#eeeefb',
         500: '#f3f3f7',
+        900: '#838383',
       },
       red: '#fff4f4',
     }
