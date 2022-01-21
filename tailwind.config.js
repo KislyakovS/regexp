3452ecaea923b78355ad78fdd401c3d873f53718
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-01-21 18:31:14 +0300

    remove style in component Tags

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx}"],
-  theme: {},
+  theme: {
+    colors: {
+      gray: {
+        100: '#f4f4ff',
+        200: '#eeeefb',
+      },
+      red: '#fff4f4',
+    }
+  },
   variants: {},
   plugins: []
 };
