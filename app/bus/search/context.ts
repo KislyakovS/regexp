184662c2b5ccf8a3000a832d02c092acc016b96c
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-02-05 16:24:13 +0300

    removed the old search logic

--- app/bus/search/context.ts
+++
@@ -1,7 +0,0 @@
-// Core
-import { createContext } from 'react';
-
-// Types
-import type { ReturnHook } from './hooks/types';
-
-export default createContext<Partial<ReturnHook>>([]);
--- app/bus/search/hooks/types.ts
+++
@@ -1,4 +0,0 @@
-// Types
-import { MouseEvent } from 'react'
-
-export type ReturnHook = [string, (event: MouseEvent<HTMLInputElement>) => void]
