e22d97df7e175da49faf9bb52294378cf0a109dc
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-02-05 15:39:51 +0300

    add utils `debounce`

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as stringToRegexp } from './string-to-regexp';
 export { default as randomNumber } from './random-number';
 export { default as searchRegexp } from './search-regexp';
+export { default as debounce } from './debounce';
